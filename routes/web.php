9d69dd155d45bc9d84007f222436f1ebbdb3cf45
bagasdamarraditya <bagasdr0106@gmail.com>
2024-05-26 22:06:47 +0700

    Praktikum 10

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,5 +18,8 @@ Route::get('/', function () {
 
 // Route untuk menampilkan teks salam
 Route::get('admin/dashboard',[DashboardController::class,'index']);
+
+// Route untuk menampilkan halaman student
+Route::get('admin/student',[StudentController::class,'index']);
 
 // Route
